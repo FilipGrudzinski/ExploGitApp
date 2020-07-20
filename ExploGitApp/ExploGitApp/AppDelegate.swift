a88dbbd10d29f084b5dde96e57d259064914aafd
FilipGrudzinski <filip.grudzinski@b-mind.co>
2020-07-20 20:32:24 +0200

    [ ] - Init project

---
+++ ExploGitApp/ExploGitApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ExploGitApp
+//
+//  Created by Filip Grudziński on 20/07/2020.
+//  Copyright © 2020 Filip Grudziński. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
